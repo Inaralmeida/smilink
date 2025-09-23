acf39fe52c6ee76b74faf38ea5b5c2adbfdc47a7
inaralmeida <inara.ralmeida@gmail.com>
2025-09-23 15:38:23 -0300

    feat: home page

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import { Typography } from "@mui/material";
+import Layout from "../../shared/components/Layout/Layout";
+
+const Home = () => {
+  return (
+    <Layout>
+      <Typography>Home</Typography>
+    </Layout>
+  );
+};
+
+export default Home;
