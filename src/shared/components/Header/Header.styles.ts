8fea3cfec67818a25ab16e38b9960f407beb0b18
inaralmeida <inara.ralmeida@gmail.com>
2025-09-24 13:50:11 -0300

    fix: add responssivines header

--- src/shared/components/Header/Header.styles.ts
+++ src/shared/components/Header/Header.styles.ts
@@ -6,11 +6,23 @@ export const HeaderContainer = styled.header`
   background-color: ${(props) => props.theme.palette.primary.main};
   box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
   color: ${(props) => props.theme.palette.primary.contrastText};
+  display: flex;
+  flex-direction: row;
+
+  @media screen and (max-width: 600px) {
+    flex-direction: column;
+    height: auto;
+  }
 `;
 export const LogoContainer = styled.div`
   display: flex;
   align-items: center;
   gap: 8px;
+
+  @media screen and (max-width: 600px) {
+    width: 100%;
+    justify-content: space-between;
+  }
 `;
 export const Logo = styled.img`
   height: 50px;
--- src/shared/components/Header/Header.tsx
+++ src/shared/components/Header/Header.tsx
@@ -1,12 +1,63 @@
+import MenuIcon from "@mui/icons-material/Menu";
+import { BottomNavigation, ToggleButton } from "@mui/material";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useState } from "react";
+import { ItensHeaderNavigation } from "../../../domain/constants/ItensHeaderNavigation";
+import IconLink from "../IconLink/IconLink";
 import Logo from "../Logo/Logo";
+import MenuSettings from "../MenuSettings/MenuSettings/MenuSettings";
 import * as S from "./Header.styles";
 
 const Header = () => {
+  const isMobile = useMediaQuery("(max-width: 600px)");
+  const [showMenu, setShowMenu] = useState(false);
+  const [currentPage, setcurrentPage] = useState("");
   return (
     <S.HeaderContainer>
       <S.LogoContainer>
         <Logo type="complete" color="white" size="sm" />
+        {isMobile && (
+          <ToggleButton
+            value="check"
+            selected={false}
+            onChange={() => setShowMenu((showMenu) => !showMenu)}
+            sx={{ border: `1px solid white` }}
+          >
+            <MenuIcon sx={{ color: "white" }} />
+          </ToggleButton>
+        )}
       </S.LogoContainer>
+
+      {((showMenu && isMobile) || !isMobile) && (
+        <BottomNavigation
+          sx={{
+            width: "100%",
+            height: "100%",
+            backgroundColor: "transparent",
+            flexDirection: isMobile ? "column" : "row",
+            flex: 1,
+          }}
+          showLabels
+          value={currentPage}
+          onChange={(_, newValue) => {
+            setcurrentPage(newValue);
+          }}
+        >
+          {ItensHeaderNavigation.map((item) => (
+            <>
+              {item.permission.admin && (
+                <IconLink
+                  Icon={item.icon}
+                  label={item.name}
+                  value={item.name}
+                  link={item.path}
+                />
+              )}
+            </>
+          ))}
+          <MenuSettings />
+        </BottomNavigation>
+      )}
     </S.HeaderContainer>
   );
 };
