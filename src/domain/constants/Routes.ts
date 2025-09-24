cbb34b952175945a06853f61614f0a4410594990
inaralmeida <inara.ralmeida@gmail.com>
2025-09-24 13:47:57 -0300

    feat: list item header

--- src/domain/constants/Routes.ts
+++ src/domain/constants/Routes.ts
@@ -4,5 +4,11 @@ export const ROUTES = {
     login: "/auth/login",
     register: "/auth/register",
   },
+  consultas: "/consultas",
+  agendar: "/agendar",
+  cadastro: "/cadastro",
+  pacientes: "/pacientes",
+  profissionais: "/profissionais",
+  estoque: "/estoque",
   notFound: "/404",
 };
