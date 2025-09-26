d18dba2edfedfd1866161f2d3007bc8d9e5489c3
inaralmeida <inara.ralmeida@gmail.com>
2025-09-25 21:22:24 -0300

    fix: remove replace private route

--- src/shared/Router/PrivateRoute.tsx
+++ src/shared/Router/PrivateRoute.tsx
@@ -1,10 +1,10 @@
 import { Navigate, Outlet } from "react-router-dom";
-import useAuth from "../../application/Hooks/useAuth";
+import { useAuth } from "../../application/context/AuthContext";
 import { ROUTES } from "../../domain/constants/Routes";
 
 const PrivateRoute = () => {
   const { isAuth } = useAuth();
-  return isAuth ? <Outlet /> : <Navigate to={ROUTES.auth.login} replace />;
+  return isAuth ? <Outlet /> : <Navigate to={ROUTES.auth.login} />;
 };
 
 export default PrivateRoute;
