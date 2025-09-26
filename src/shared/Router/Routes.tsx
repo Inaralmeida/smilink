1db76472302fc7d439db994e2179adc6eaed7ed5
inaralmeida <inara.ralmeida@gmail.com>
2025-09-25 21:24:17 -0300

    feat: login page

--- src/shared/Router/Routes.tsx
+++ src/shared/Router/Routes.tsx
@@ -2,8 +2,10 @@ import { CssBaseline, Typography } from "@mui/material";
 import { ThemeProvider as MuiThemeProvider } from "@mui/material/styles";
 import { BrowserRouter, Route, Routes as Switch } from "react-router-dom";
 import { ThemeProvider as SCThemeProvider } from "styled-components";
+import { AuthProvider } from "../../application/context/AuthContext";
 import { ROUTES } from "../../domain/constants/Routes";
 import { Home, Pacientes } from "../../pages";
+import Login from "../../pages/login/Login";
 import { Layout } from "../components";
 import { theme } from "../ui/style/theme";
 import PrivateRoute from "./PrivateRoute";
@@ -11,30 +13,29 @@ const Routes = () => {
   return (
     <MuiThemeProvider theme={theme}>
       <SCThemeProvider theme={theme}>
-        <BrowserRouter>
-          <CssBaseline />
+        <AuthProvider>
+          <BrowserRouter>
+            <CssBaseline />
 
-          <Switch>
-            <Route element={<PrivateRoute />}>
-              <Route element={<Layout />}>
-                <Route path={ROUTES.home} element={<Home />} />
-                <Route path={ROUTES.pacientes} element={<Pacientes />} />
-                <Route
-                  path="*"
-                  element={<Typography variant="h1">404</Typography>}
-                />
+            <Switch>
+              <Route element={<PrivateRoute />}>
+                <Route element={<Layout />}>
+                  <Route path={`${ROUTES.home}/:role`} element={<Home />} />
+                  <Route path={ROUTES.pacientes} element={<Pacientes />} />
+                  <Route
+                    path="*"
+                    element={<Typography variant="h1">404</Typography>}
+                  />
+                </Route>
               </Route>
-            </Route>
-            <Route
-              path={ROUTES.auth.login}
-              element={<Typography variant="h1">login</Typography>}
-            />
-            <Route
-              path={ROUTES.notFound}
-              element={<Typography variant="h1">404</Typography>}
-            />
-          </Switch>
-        </BrowserRouter>
+              <Route path={ROUTES.auth.login} element={<Login />} />
+              <Route
+                path={ROUTES.notFound}
+                element={<Typography variant="h1">404</Typography>}
+              />
+            </Switch>
+          </BrowserRouter>
+        </AuthProvider>
       </SCThemeProvider>
     </MuiThemeProvider>
   );
