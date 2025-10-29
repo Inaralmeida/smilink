36fcb40c3a0aad78c10212dd971a9aa55f2358b8
almeidainara <inara.almeida@creditas.com>
2025-10-29 06:04:16 -0300

    feat: public route

--- src/shared/Router/Routes.tsx
+++ src/shared/Router/Routes.tsx
@@ -2,25 +2,27 @@ import { CssBaseline, Typography } from "@mui/material";
 import { ThemeProvider as MuiThemeProvider } from "@mui/material/styles";
 import { BrowserRouter, Route, Routes as Switch } from "react-router-dom";
 import { ThemeProvider as SCThemeProvider } from "styled-components";
-import { AuthProvider } from "../../application/context/AuthContext";
 import { ROUTES } from "../../domain/constants/Routes";
 import { Home, Pacientes } from "../../pages";
-import Login from "../../pages/login/Login";
 import { Layout } from "../components";
 import { theme } from "../ui/style/theme";
 import PrivateRoute from "./PrivateRoute";
+import PublicRoute from "./PublicRoute"; // 1. IMPORTE O NOVO COMPONENTE
+import { AuthProvider } from "../../application/context/AuthContext";
+import Login from "../../pages/login/Login";
+
 const Routes = () => {
   return (
     <MuiThemeProvider theme={theme}>
       <SCThemeProvider theme={theme}>
-        <AuthProvider>
-          <BrowserRouter>
-            <CssBaseline />
-
+        <BrowserRouter>
+          <CssBaseline />
+          <AuthProvider>
             <Switch>
+              {/* --- Rotas Protegidas (Só para logados) --- */}
               <Route element={<PrivateRoute />}>
                 <Route element={<Layout />}>
-                  <Route path={`${ROUTES.home}/:role`} element={<Home />} />
+                  <Route path={ROUTES.home} element={<Home />} />
                   <Route path={ROUTES.pacientes} element={<Pacientes />} />
                   <Route
                     path="*"
@@ -28,14 +30,20 @@ const Routes = () => {
                   />
                 </Route>
               </Route>
-              <Route path={ROUTES.auth.login} element={<Login />} />
+
+              {/* --- Rotas Públicas (Só para NÃO logados) --- */}
+              <Route element={<PublicRoute />}>
+                <Route path={ROUTES.auth.login} element={<Login />} />
+              </Route>
+
+              {/* Rota 404 geral (pode ser ajustada) */}
               <Route
                 path={ROUTES.notFound}
                 element={<Typography variant="h1">404</Typography>}
               />
             </Switch>
-          </BrowserRouter>
-        </AuthProvider>
+          </AuthProvider>
+        </BrowserRouter>
       </SCThemeProvider>
     </MuiThemeProvider>
   );
